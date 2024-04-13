15c7cd9f718fd054409c89ad412811410e311050
smose94 <simon_moseley55@yahoo.co.uk>
2024-04-13 15:11:37 +0100

    add add folder

---
+++ frontend/data/teamabbreviations.js
@@ -0,0 +1,40 @@
+// teamColours.js
+// Example structure. Adjust it according to your actual file
+const teamAbbr = {
+    "Anaheim Ducks": "ANA", // Example team name and its color
+    "Arizona Coyotes": "ARI",
+    "Boston Bruins": "BOS",
+    "Buffalo Sabres": "BUF",
+    "Calgary Flames": "CGY",
+    "Carolina Hurricanes": "CAR",
+    "Chicago Blackhawks": "CHI",
+    "Colorado Avalanche": "COL",
+    "Columbus Blue Jackets": "CBJ",
+    "Dallas Stars": "DAL",
+    "Detroit Red Wings": "DET",
+    "Edmonton Oilers": "EDM",
+    "Florida Panthers": "FLA",
+    "Los Angeles Kings": "LAK",
+    "Minnesota Wild": "MIN", 
+    "Montreal Canadiens": "MTL",
+    "Nashville Predators": "NSH",
+    "New Jersey Devils": "NJD",
+    "New York Islanders": "NYI",
+    "New York Rangers": "NYR",
+    "Ottawa Senators": "OTT",
+    "Philadelphia Flyers": "PHI",
+    "Pittsburgh Penguins": "PIT",
+    "San Jose Sharks": "SJS",
+    "Seattle Kraken": "SEA",
+    "St. Louis Blues": "STL",
+    "Tampa Bay Lightning": "TBL",
+    "Toronto Maple Leafs":"TOR",
+    "Vancouver Canucks": "VAN",
+    "Vegas Golden Knights": "VGK", 
+    "Washington Capitals": "WSH",
+    "Winnipeg Jets": "WPG"
+    // Add all teams and their colors
+  };
+  
+  export default teamAbbr;
+
---
+++ frontend/data/teamcolours.js
@@ -0,0 +1,40 @@
+// teamColours.js
+// Example structure. Adjust it according to your actual file
+const teamColours = {
+    "Anaheim Ducks": "#F47A38", // Example team name and its color
+    "Arizona Coyotes": "#8C2633",
+    "Boston Bruins": "#FFB81C",
+    "Buffalo Sabres": "#002654",
+    "Calgary Flames": "#C8102E",
+    "Carolina Hurricanes": "#CC0000",
+    "Chicago Blackhawks": "#CF0A2C",
+    "Colorado Avalanche": "#6F263D",
+    "Columbus Blue Jackets": "#002654",
+    "Dallas Stars": "#006847",
+    "Detroit Red Wings": "#CE1126",
+    "Edmonton Oilers": "#FF4C00",
+    "Florida Panthers": "#B9975B",
+    "Los Angeles Kings": "#111111",
+    "Minnesota Wild": "#154734", 
+    "Montreal Canadiens": "#AF1E2D",
+    "Nashville Predators": "#FFB81C",
+    "New Jersey Devils": "#CE1126",
+    "New York Islanders": "#F47D30",
+    "New York Rangers": "#0038A8",
+    "Ottawa Senators": "#C52032",
+    "Philadelphia Flyers": "#F74902",
+    "Pittsburgh Penguins": "#000000",
+    "San Jose Sharks": "#002F87",
+    "Seattle Kraken": "#006D75",
+    "St. Louis Blues": "#001628",
+    "Tampa Bay Lightning": "#002868",
+    "Toronto Maple Leafs":"#00205B",
+    "Vancouver Canucks": "#00205B",
+    "Vegas Golden Knights": "#B4975A", 
+    "Washington Capitals": "#C8102E",
+    "Winnipeg Jets": "#041E42"
+    // Add all teams and their colors
+  };
+  
+  export default teamColours;
+
